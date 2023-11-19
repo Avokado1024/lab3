193f215c9b11ff9591e2890b419c867013f10850
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2023-11-19 22:16:24 +0300

    Update laba 3 9.cpp

--- laba 3 9.cpp
+++ laba 3 9.cpp
@@ -6,14 +6,13 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	string str1;
 	getline(cin, str1);
-	vector<char> v1(str1.begin(), str1.end());
 	int count = 0;
-	for (char f : v1) {
+	for (char f : str1) {
 		if (f == 'a' or f == 'A') {
 			++count;
 		}
 	}
-	double num = (count * 100) / v1.size();
+	double num = (count * 100) / str1.size();
 	cout << num<<"%";
 
 }
