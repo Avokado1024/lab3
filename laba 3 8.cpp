5bfaf4bbaad16cce12e84e2cb3ef740503aa108b
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2023-11-19 22:13:58 +0300

    Update laba 3 8.cpp

--- laba 3 8.cpp
+++ laba 3 8.cpp
@@ -1,22 +1,15 @@
 ﻿using namespace std;
 #include <vector>
-#include <iostream>
 #include <string>
+#include <iostream>
 int main() {
-	string str1;
-	getline(cin, str1);
-	vector<char> v1(str1.begin(), str1.end());
-	vector<char> v2;
-	for (int i = 0; i < v1.size(); ++i) {
-		v2.push_back('*');
-	}
-	for (int i = 0; i < v1.size(); ++i) {
-		v2.push_back(v1[i]);
-	}
-	for (int i = 0; i < v1.size(); ++i) {
-		v2.push_back('*');
-	}
-	for (char f : v2) {
-		cout << f << " ";
-	}
+ string word;
+ cin >> word;
+ for (int i = 0; i < word.length(); ++i) {
+  cout << "*";
+ }
+ cout << word;
+ for (int i = word.length(); i < 2 * word.length(); ++i) {
+  cout << "*";
+ }
 }
