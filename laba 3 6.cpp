9491a643895d32261fecb2f342e60a30eb84035d
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2023-11-19 22:07:09 +0300

    Update laba 3 6.cpp

--- laba 3 6.cpp
+++ laba 3 6.cpp
@@ -1,18 +1,16 @@
 ﻿using namespace std;
-#include <vector>
 #include <iostream>
 #include <string>
-int main() {
-	string str1;
-	string str2;
-	getline(cin, str1);
-	getline(cin, str2);
-	vector<char> v1(str1.begin(), str1.end());
-	vector<char> v2(str2.begin(), str2.end());
-	if (str1.size()> str2.size()) {
-		cout << "true";
-	}
-	else {
-		cout << "false";
-	}
+int main()
+{
+ string f1;
+ string f2;
+ cin >> f1;
+ cin >> f2;
+ if (f1.length() > f2.length()) {
+  cout << "true" << endl;
+ }
+ else {
+  cout << "false" << endl;
+ }
 }
