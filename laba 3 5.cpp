973e869ddcb5ebee56cafa251d71a56bbd6a9cc3
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2023-11-19 22:05:54 +0300

    Update laba 3 5.cpp

--- laba 3 5.cpp
+++ laba 3 5.cpp
@@ -1,19 +1,12 @@
-﻿using namespace std;
-#include <vector>
 #include <iostream>
 #include <string>
+using namespace std;
 int main(){
-	string str;
-	getline(cin, str);
-	vector<char> v1(str.begin(), str.end());
-	for (char f : v1) {
-		cout << f << " ";
-	}
-	cout << endl;
-	if (str.size() % 2 == 0) {
-		cout << "chetno";
-	}
-	else {
-		cout << "nechetno";
-	}
-}
+ string city;
+ cin >> city;
+ if (city.length() % 2 == 0) {
+  cout << "chetno";
+ }
+ else {
+  cout << "nechetno" << endl;
+ }}
