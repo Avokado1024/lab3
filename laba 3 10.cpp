67d471ed4ee38396c8dbf997dbca8e23400f8805
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2023-11-19 22:27:08 +0300

    Update laba 3 10.cpp

--- laba 3 10.cpp
+++ laba 3 10.cpp
@@ -1,23 +1,17 @@
-﻿using namespace std;
-#include <vector>
 #include <iostream>
+using namespace std;
 #include <string>
 int main() {
 	setlocale(LC_ALL, "Russian");
-	string str= " Can you can a can as a canner can can a can?";
-	vector<char> v1(str.begin(), str.end());
-	vector<char> v2;
-	int i = 1;
+	string v2{""};
+    string v1{ " Can you can a can as a canner can can a can?"};
+    string n;
+    getline(cin, n);
+    int i = 1;
 	while (i < v1.size()) {
 		if ((v1[i] == 'c' or v1[i] == 'C') and v1[i + 1] == 'a' and v1[i + 2] == 'n' and v1[i - 1] == ' ' and (v1[i + 3] == ' ' or v1[i + 3] == '?')) {
-			v2.push_back('n');
-			v2.push_back('e');
-			v2.push_back('w');
-			v2.push_back('_');
-			v2.push_back('w');
-			v2.push_back('o');
-			v2.push_back('r');
-			v2.push_back('d');
+		for(char f:n){
+			v2.push_back(f);}
 			i = i + 3;
 		}
 		else {
